611df8c61be7e654a46fcf83668c996da2226f7a
laila <hwaijilaila@gmail.com>
2019-01-28 21:44:46 +0200

    third assignment

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ function FoundItemsDirective() {
 
 function ShoppingListDirectiveController() {
   var list = this;
+  list.items=[];
+  // list.error = list.items.length ===0 ?"nothing found" : "";
 
 }
 
@@ -38,34 +40,55 @@ function NarrowItDownController(MenuCategoriesService) {
     var NarDownCTRL = this;
   NarDownCTRL.searchFor = "";
 
+NarDownCTRL.showError=false;
+  
 
-  NarDownCTRL.found = [];
-
-
+ 
   NarDownCTRL.getAll = function(){
-    console.log("i am");
-    NarDownCTRL.found=[];
-    var promise = MenuCategoriesService.getMenuCategories();
-
-    promise.then(function (response) {
-
-      
-       for (var i = 0; i < response.data.length ; i++) {
-          var namedesc = response.data[i].name;
-          var insdes = response.data[i].special_instructions;
-
-          if((namedesc.toLowerCase().indexOf(NarDownCTRL.searchFor)!= - 1)
-            || (insdes.toLowerCase().indexOf(NarDownCTRL.searchFor.toLowerCase())!= - 1)){
-           
-              NarDownCTRL.found.push(response.data[i]);
-           }
-       }
-      
-      })
-      .catch(function (error) {
-        console.log("Something went terribly wrong.");
-      });
+    NarDownCTRL.found = MenuCategoriesService.getMenuCategories(NarDownCTRL.searchFor);
+  console.log(NarDownCTRL.found);
   }
+
+    NarDownCTRL.getAll = function () {
+    MenuCategoriesService.getMenuCategories(NarDownCTRL.searchFor.toLowerCase())
+    .then(function (response) {
+       NarDownCTRL.found = response;
+
+      if ( NarDownCTRL.found.length) {
+        NarDownCTRL.showError = false;
+      } else {
+
+        NarDownCTRL.showError = true;
+        console.log(NarDownCTRL.showError);
+      }
+    })
+    .catch(function (error) {
+      console.log("Something went terribly wrong.");
+    });
+  };
+  //   console.log("i am");
+  //   NarDownCTRL.found=[];
+  //   var promise = MenuCategoriesService.getMenuCategories();
+
+  //   promise.then(function (response) {
+
+      
+  //      for (var i = 0; i < response.data.length ; i++) {
+  //         var namedesc = response.data[i].name;
+  //         var insdes = response.data[i].special_instructions;
+
+  //         if((namedesc.toLowerCase().indexOf(NarDownCTRL.searchFor)!= - 1)
+  //           || (insdes.toLowerCase().indexOf(NarDownCTRL.searchFor.toLowerCase())!= - 1)){
+           
+  //             NarDownCTRL.found.push(response.data[i]);
+  //          }
+  //      }
+      
+  //     })
+  //     .catch(function (error) {
+  //       console.log("Something went terribly wrong.");
+  //     });
+  // }
 
   NarDownCTRL.removeItem = function (itemIndex) {
       NarDownCTRL.found.splice(itemIndex,1);
@@ -79,13 +102,19 @@ MenuCategoriesService.$inject = ['$http', 'ApiBasePath'];
 function MenuCategoriesService($http, ApiBasePath) {
   var service = this;
 
-  service.getMenuCategories = function () {
-    var response = $http({
+  service.getMenuCategories = function (searchTerm) {
+    return $http({
       method: "GET",
-      url: (ApiBasePath + "/categories.json")
+      url: (ApiBasePath + "/menu_items.json"),
+    })
+    .then(function (response) {
+      var foundItems = response.data.menu_items;
+      if (searchTerm === "") {
+        return [];
+      } else {
+        return foundItems.filter(item=>item.description.includes(searchTerm));
+      }
     });
-
-    return response;
   };
 
 
@@ -95,7 +124,4 @@ function MenuCategoriesService($http, ApiBasePath) {
 
 
 })();
-
-
-
 
